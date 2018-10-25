767aa6d0597fbba293edd83b802f4f8bb391eb2c
condef5 <davis.con.fab@gmail.com>
2018-10-25 17:29:22 -0500

    state

---
+++ fcc.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const session     = require('express-session');
+const mongo       = require('mongodb').MongoClient;
+const routes      = require('./routes');
+const auth        = require('./auth');
+
+const app = express();
+
+fccTesting(app); //For FCC testing purposes
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug')
+
+mongo.connect(process.env.DATABASE, (err, db) => {
+    if(err) {
+        console.log('Database error: ' + err);
+    } else {
+        console.log('Successful database connection');
+        
+        auth(app, db);
+        
+        routes(app, db);
+      
+        app.listen(process.env.PORT || 3000, () => {
+          console.log("Listening on port " + process.env.PORT);
+        });  
+}});
