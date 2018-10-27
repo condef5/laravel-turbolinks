110cb14fb59854afa12a3b4113a313fd1c51b00d
condef5 <davis.con.fab@gmail.com>
2018-10-27 16:14:43 -0500

    Add styles and create errors template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,6 +63,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="/animes">Animes</a>
+                                    <a class="dropdown-item" href="/animes/create">Create anime</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
---
+++ resources/views/partials/errors.blade.php
@@ -0,0 +1,11 @@
+@if (count($errors))
+    <div class="form-group">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+@endif
