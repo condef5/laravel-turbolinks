cedc33b1154f189ee93636ba406d1f5fede865a7
condef5 <davis.con.fab@gmail.com>
2018-10-27 16:25:18 -0500

    Add handy-notification

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,12 @@
 import  Turbolinks from 'turbolinks';
 import './bootstrap';
 import vue from 'vue';
+import Notify from 'handy-notification'
 
 Turbolinks.start()
 
 window.Vue = vue
+window.Notify = Notify
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
